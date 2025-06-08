163cf2df83b83bd8747e1357cbb35a4e3193ccfd
Marc Beninca <git@marc.beninca.link>
2025-06-08 20:58:45 +0200

    out

--- render.py
+++ render.py
@@ -6,6 +6,6 @@ from pathlib import Path
 from rwx.fs import make_directory, write
 
 if __name__ == "__main__":
-    out = Path(__file__).parent / "output" / "web"
+    out = Path(__file__).parent / "out" / "web"
     make_directory(out)
     write(out / "index.html", "prj.rwx.work")
